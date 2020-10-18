0a2ec5709a07bbda52adf75b4cc5db91d0de23c0
kboczon <kamil.boczon@live.com>
2020-10-18 11:35:32 -0500

    Initial commit

---
+++ setupDisplay.py
@@ -0,0 +1,12 @@
+import subprocess
+import re
+
+eth0ip = subprocess.Popen(['ip', 'a'], stdout=subprocess.PIPE)
+eth0grep = subprocess.Popen(['grep','eth0'], stdin=eth0ip.stdout, stdout=subprocess.PIPE)
+eth0inet = subprocess.Popen(['grep','inet'], stdin=eth0grep.stdout, stdout=subprocess.PIPE)
+outval = eth0inet.communicate()[0].decode('utf8')
+reg_pat=r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.)"
+outval_reg = re.search(reg_pat,outval)
+displVal = outval_reg.group()
+displVal = f"{displVal}1:0"
+print(displVal)
